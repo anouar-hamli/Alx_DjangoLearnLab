d3d98c83af1b7cd158d03b8b76b87a342733768b
anouar hamli <anouarhamli10@gmail.com>
2025-07-20 23:33:56 +0100

    good

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import user_passes_test, login_required
+from django.shortcuts import render
 
-# Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+    
+    
+
+# View تسجيل الدخول
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('list_books')  # أو أي صفحة تريد إعادة التوجيه إليها بعد تسجيل الدخول
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+# View تسجيل الخروج
+@login_required
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
+
+# View التسجيل (إنشاء مستخدم جديد)
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # تسجيل الدخول تلقائياً بعد التسجيل
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+
+def role_check(role):
+    def check(user):
+        return hasattr(user, 'userprofile') and user.userprofile.role == role
+    return check
+
+@login_required
+@user_passes_test(role_check('Admin'))
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@login_required
+@user_passes_test(role_check('Librarian'))
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@login_required
+@user_passes_test(role_check('Member'))
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
